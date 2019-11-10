8e4dec0095bc0026ec66e37196f4e4ca3b87d117
brian <riceb53@gmail.com>
2019-11-10 11:56:09 -0600

    generate model, add to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dog = Dog.new(name: "clifford", age: 10, breed: "large, red")
+
+dog.save
